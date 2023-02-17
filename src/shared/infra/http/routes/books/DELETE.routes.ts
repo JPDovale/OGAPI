a69a96fe400fd47a73799fe388456f2e97081395
JPDovale <joaopaulodovaledeo.60@gmail.com>
2023-02-17 18:05:27 -0300

    add complete, delete and update scenes

--- src/shared/infra/http/routes/books/DELETE.routes.ts
+++ src/shared/infra/http/routes/books/DELETE.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express'
 
+import { DeleteSceneController } from '@modules/books/useCases/DeleteScene/DeleteSceneController'
 import { RemoveFrontCoverController } from '@modules/books/useCases/RemoveFrontCover/RemoveFrontCoverController'
 
 export const booksRoutesDelete = Router()
 
 const removeFrontCoverController = new RemoveFrontCoverController()
+const deleteSceneController = new DeleteSceneController()
 
 booksRoutesDelete.delete(
   '/front-cover/:bookId',
   removeFrontCoverController.handle,
+)
+
+booksRoutesDelete.delete(
+  '/:bookId/capitules/:capituleId/scenes/:sceneId',
+  deleteSceneController.handle,
 )
--- src/shared/infra/http/routes/books/PUT.routes.ts
+++ src/shared/infra/http/routes/books/PUT.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express'
 
+import { ReorderScenesController } from '@modules/books/useCases/ReorderScenes/ReorderScenesController'
 import { SetCompleteSceneController } from '@modules/books/useCases/SetCompleteScene/SetCompleteSceneController'
 import { UpdateCapituleController } from '@modules/books/useCases/UpdateCapitule/UpdateCapituleController'
+import { UpdateSceneController } from '@modules/books/useCases/UpdateScene/UpdateSceneController'
 
 export const booksRoutesPut = Router()
 
 const updateCapituleController = new UpdateCapituleController()
 const setCompleteSceneController = new SetCompleteSceneController()
+const reorderScenesController = new ReorderScenesController()
+const updateSceneController = new UpdateSceneController()
 
 booksRoutesPut.put('/capitules', updateCapituleController.handle)
 booksRoutesPut.put(
   '/capitules/scenes/complete',
   setCompleteSceneController.handle,
 )
+booksRoutesPut.put('/capitules/scenes/reorder', reorderScenesController.handle)
+booksRoutesPut.put('/capitules/scenes', updateSceneController.handle)
