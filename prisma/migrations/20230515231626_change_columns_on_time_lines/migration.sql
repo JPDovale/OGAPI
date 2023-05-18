850cd4e811314ad97908ed6619598898ba96acb8
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-05-18 15:07:22 -0300

    CorrectDatabase

--- prisma/migrations/20230515231626_change_columns_on_time_lines/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `time_lines` MODIFY `title` VARCHAR(191) NULL,
-    MODIFY `description` VARCHAR(600) NULL;
--- prisma/migrations/20230515232634_update_requirement_on_columns_of_project/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `projects` MODIFY `initial_date_time_christ` VARCHAR(191) NOT NULL DEFAULT 'D.C.';
