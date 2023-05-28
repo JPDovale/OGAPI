8453366a05ad16a8e99d6a8d5a806237a4e8ff33
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-05-27 23:26:37 -0300

    update cors another time

--- src/shared/infra/http/index.ts
+++ src/shared/infra/http/index.ts
@@ -37,11 +37,26 @@ const rateLimit = new RateLimiter({ limit: 50, per: 'minutes' })
 
 app.use(
   cors({
-    allowedHeaders: '*',
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'Accept-Language',
+      'Accept-Encoding',
+      'Origin',
+      'Referer',
+      'Cookie',
+      'Sec-Fetch-Dest',
+      'Sec-Fetch-Mode',
+      'Sec-Fetch-Site',
+      'Pragma',
+      'Cache-Control',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     origin: [
       'https://www.ognare.com.br',
       'https://ognare.com.br',
+      'http://localhost',
       appTest ?? '',
     ],
     preflightContinue: false,
